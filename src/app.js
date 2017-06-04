ae6defaf7ce14cc943d6c7db9f7dd7809fc290d3
Jezrael Arciaga <jezarciaga@gmail.com>
2017-06-04 09:16:02 +0800

    Add link to resume

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ let article = $(`
     <div>
         <h2>Jezrael Arciaga</h2>
         <div>Data Scientist | Data Engineer | Software Engineer</p>
-        <div id="resume"><a class="border-white" href="">View Resume</a></p>
+        <div id="resume"><a class="border-white" href="Jezrael-Arciaga-CV.pdf">View Resume</a></p>
     </div>
 </article>
 `).appendTo(section);
