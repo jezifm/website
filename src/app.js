9740691b8269872208d3ca937424c0724c3ca4ab
Jezrael Arciaga <jezarciaga@gmail.com>
2017-06-04 11:31:09 +0800

    Refactor remove jQuery

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
 // core
 require('./style.scss');
 
-import $ from 'jquery';
-
-
-
-let section = $('<section></section>').appendTo('body')
-
-let article = $(`
-<article>
-    <div>
-        <h2>Jezrael Arciaga</h2>
-        <div>Data Scientist | Data Engineer | Software Engineer</p>
-        <div id="resume"><a class="border-white" href="Jezrael-Arciaga-CV.pdf">View Resume</a></p>
-    </div>
-</article>
-`).appendTo(section);
+document.write(`
+<section>
+    <article>
+        <div>
+          <h2>Jezrael Arciaga</h2>
+          <div>Data Scientist | Data Engineer | Software Engineer</p>
+          <div id="resume"><a class="border-white" href="Jezrael-Arciaga-CV.pdf">View Resume</a></p>
+        </div>
+    </article>
+</section>
+`)
