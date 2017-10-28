e276c43b87c15eb263b90974c3508df33ce4d151
Jezrael Arciaga <jezarciaga@gmail.com>
2017-10-28 17:13:58 +0800

    remove snap scrolling - not snappy enough

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ require('./style.scss')
 let body = require('./index.pug')
 document.write(body)
 
-// TODO: optimize feature - snap scroll
-//       jquery solution - adds 100kb. too large
-let $ = require('jquery')
-import 'jquery-scrollify'
-$(() => $.scrollify({section: 'article'}))
+// TODO: snap scrolling
+//       jquery solution
+//         - adds 100kb. too large
+//         - not snappy enough
+// let $ = require('jquery')
+// import 'jquery-scrollify'
+// $(() => $.scrollify({section: 'article'}))
