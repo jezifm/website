672b41e59515ed8757e018abd6ce57e267d9586d
Jezrael Arciaga <jezarciaga@gmail.com>
2017-10-28 17:11:55 +0800

    enable scroll snapping

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 // core
-require('./style.scss');
+require('./style.scss')
 
 let body = require('./index.pug')
 document.write(body)
 
-// TODO: implement snap scroll
+// TODO: optimize feature - snap scroll
 //       jquery solution - adds 100kb. too large
-//
-// let $ = require('jquery')
-// import 'jquery-scrollify'
-// $(() => $.scrollify({section: 'article'}))
+let $ = require('jquery')
+import 'jquery-scrollify'
+$(() => $.scrollify({section: 'article'}))
